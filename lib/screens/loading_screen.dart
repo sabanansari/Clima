2ef47355dabba6904878ad6bde4dc572b3e92f61
sabanansari <alisaban52@gmail.com>
2020-07-24 00:03:48 +0530

    http introduced to access api

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:get_weather/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,9 +10,34 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  Location location = Location();
+  double longitude;
+  double latitude;
+
   void getLocation() async {
-    Position position = await Geolocator()
-        .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    await location.getCurrentLocation();
+    longitude = location.longitude;
+    latitude = location.latitude;
+  }
+
+  void getData() async {
+    http.Response response = await http.get(
+        'https://samples.openweathermap.org/data/2.5/weather?lat=latitude&lon=longitude&appid=439d4b804bc8187953eb36d2a8c26a02');
+    if (response.statusCode == 200) {
+      String data = response.body;
+      var decode = jsonDecode(data);
+      double temp = decode['main']['temp'];
+      int condition = decode['weather'][0]['id'];
+      String cityName = decode['name'];
+    } else {
+      print(response.statusCode);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
   }
 
   @override
